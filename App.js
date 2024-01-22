df95cb8255d182213bded82b4fb175b370183c15
Rafael Felipe Reis Altino <rafaelfraltino@gmail.com>
2024-01-22 01:13:17 -0300

    Commit inicial

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Cesta from './src/telas/Cesta';
+import {
+  useFonts, 
+  Montserrat_400Regular, 
+  Montserrat_700Bold
+} from '@expo-google-fonts/montserrat';
+
+export default function App() {
+  
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  })
+
+  if(!fonteCarregada){
+    return <View/>
+  }
+
+  return (
+    <SafeAreaView>
+      <StatusBar/>
+      <Cesta/> 
+    </SafeAreaView>
+  );
+}
+
